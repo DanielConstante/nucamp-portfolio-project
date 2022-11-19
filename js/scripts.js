a704d7a7ff9141363b01612a3fe00ed190093d7b
Daniel Constante <daniel_constante86@hotmail.com>
2022-11-18 18:54:00 -0600

    New style

--- js/scripts.js
+++ js/scripts.js
@@ -9,5 +9,17 @@ $(function () {
 });
 
 
+   //loading spinner 
+   var myVar;
 
+   function myFunction() {
+       myVar = setTimeout(showPage, 2000);
+   }
+
+   function showPage() {
+       document.getElementById("loader").style.display = "none";
+       document.getElementById("myDiv").style.display = "block";
+       document.getElementById("title-container").style.display = "none";
+       document.getElementById("title").style.display = "none";
+    }
 
